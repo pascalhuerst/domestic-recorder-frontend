703566ff14d212f46c0dff154b2ef4d461794f03
Pascal Huerst <pascal.huerst@gmail.com>
2020-11-13 16:01:00 +0100

    Be able to handle different recorders

--- src/components/OpenSessions.vue
+++ src/components/OpenSessions.vue
@@ -1,42 +1,47 @@
 <template>
   <section class="section">
     <div class="sessions">
-      <h1 class="title">Available Sessions</h1>
       <div
-        class="session"
-        v-for="(session, index) in sessionsList"
-        :key="session.ID"
+        class="recorder"
+        v-for="(recorder, key) in sessionsList"
+        :key="recorder"
       >
-        
-        <h2 class="subtitle is-2">Session #{{ index }}</h2>
-        <nav class="level">
-        <!-- Left side -->
-          <div class="level-left">
-            <div class="level-item">
-              <div class="tags has-addons">
-                <span class="tag">Lifetime</span>
-                <span class="tag is-primary">{{ formatHourToLive(session.hours_to_live)}} Hours</span>
-              </div>              
+        <h2 class="subtitle is-2"><strong>Recorder:</strong> {{ key }}</h2>
+        <div
+        class="session"
+        v-for="(session, index) in recorder.open_sessions"
+        :key="index"
+      >
+          <h3 class="subtitle is-3">Session #{{ index }}</h3>
+          <nav class="level">
+          <!-- Left side -->
+            <div class="level-left">
+              <div class="level-item">
+                <div class="tags has-addons">
+                  <span class="tag">Lifetime</span>
+                  <span class="tag is-primary">{{ formatHourToLive(session.hours_to_live)}} Hours</span>
+                </div>              
+              </div>
+              <div class="level-item">
+                <div class="tags has-addons">
+                  <span class="tag">Created</span>
+                  <span class="tag is-primary">{{ new Date(session.timestamp).toLocaleString() }}</span>
+                </div>    
+              </div>
             </div>
-            <div class="level-item">
-              <div class="tags has-addons">
-                <span class="tag">Created</span>
-                <span class="tag is-primary">{{ new Date(session.timestamp).toLocaleString() }}</span>
-              </div>    
-            </div>
-          </div>
 
-          <!-- Right side -->
-          <div class="level-right">
-          </div>
-        </nav>
-        
-        <a v-bind:href="serverURL + 'detail/domestic-recorder-3/' + session.id">
-          <img
-            class="waveform"
-            :src="backendServerURL +  'domestic-recorder-3' + '/' + session.id + '/' + 'overview.png'"
-          />
-        </a>
+            <!-- Right side -->
+            <div class="level-right">
+            </div>
+          </nav>
+
+          <a v-bind:href="serverURL + 'detail/' + key + '/' + session.id">
+            <img
+              class="waveform"
+              :src="backendServerURL +  key + '/' + session.id + '/' + 'overview.png'"
+            />
+          </a>
+      </div>
       </div>
     </div>
   </section>
@@ -52,7 +57,7 @@ export default {
     return {
       serverURL: "http://server.lan:8080/",
       backendServerURL: "http://server.lan:8234/",
-      sessionsList: [],
+      sessionsList: {},
       selectedAudioFileURL: '',
       selectedArrayBufferURL: '',
     };
@@ -66,7 +71,7 @@ export default {
       fetch(this.backendServerURL + 'introspect')
         .then((response) => response.json())
         .then((data) => {
-          this.sessionsList = data["domestic-recorder-3"].open_sessions;
+          this.sessionsList = data;
           console.log(JSON.stringify(this.sessionsList));
         });
     },
